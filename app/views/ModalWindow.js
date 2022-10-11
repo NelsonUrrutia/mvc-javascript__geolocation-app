1aedac8c822a8b2de33f3fa788a2f4f6043f990e
NelsonEUrrutia <85139793+NelsonEUrrutia@users.noreply.github.com>
2022-10-11 08:53:05 -0600

    Added toat HTML & CSS

--- app/views/ModalWindow.js
+++ app/views/ModalWindow.js
@@ -18,6 +18,8 @@ class ModalWindow{
  *      →Delete: mostrar franja roja
  *      →Edit: mostrar franja naranja
  *      →Success: mostrar "toast" en parte inferior derecha
+ *          Hacer animacion para mostrar y esconder utilizando CSS
+ *      Mostrar iconos: error, success
  * 3. Devolver una respuesta (true/false) cuando el modal tenga controles
  * 4. Utilizar promesas en el controlador para esperar el click de los botones
  * 5. Utilizar una clase para el markup y los eventos click 
