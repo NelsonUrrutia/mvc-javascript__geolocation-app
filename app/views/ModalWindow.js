293090588d192d6f4491d0e63f7d89fb2b41f4f0
Nelson E. Urrutia <nelurrutia15@gmail.com>
2022-10-11 07:42:51 -0600

    Added todo points on ModalWindow

---
+++ app/views/ModalWindow.js
@@ -0,0 +1,24 @@
+class ModalWindow{
+
+
+    #main_container = document.querySelector("#global__window");
+    #custom_text = document.querySelector("#global__window");
+    #delete_button = document.querySelector("#delete");
+    #cancel_button = document.querySelector("#cancel_delete");
+
+
+
+
+}
+
+/**
+ * Funcionalidades
+ * 1. Recibir texto para mostrar
+ * 2. Recibir el tipo de modal 
+ *      →Delete: mostrar franja roja
+ *      →Edit: mostrar franja naranja
+ *      →Success: mostrar "toast" en parte inferior derecha
+ * 3. Devolver una respuesta (true/false) cuando el modal tenga controles
+ * 4. Utilizar promesas en el controlador para esperar el click de los botones
+ * 5. Utilizar una clase para el markup y los eventos click 
+ */
