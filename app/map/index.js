4e4492e01659993c49a547260491a64a2bbc1388
NelsonEUrrutia <85139793+NelsonEUrrutia@users.noreply.github.com>
2022-10-30 14:50:44 -0600

    Added modal intro

--- app/map/index.js
+++ app/map/index.js
@@ -4,6 +4,7 @@ import {init_render_workouts} from "./controllers/_render_workout.js"
 import { init_sort_menu } from "./controllers/_sort_menu.js";
 import { init_modal_window } from "./controllers/_modal_windows.js";
 import { init_mobile_menu } from "./controllers/_mobile_sort_menu.js";
+import { show_intro_modal } from "./controllers/_intro_modal.js";
 
 init_map_controller();
 init_workout_form();
@@ -11,6 +12,7 @@ init_render_workouts();
 init_sort_menu();
 init_modal_window();
 init_mobile_menu();
+show_intro_modal();
 
 /**
 TODO:
